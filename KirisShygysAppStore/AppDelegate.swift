5ff56e1e7ecf0ccc026a194b516573601faa0716
Nurdaulet <abdikaimovnurdaulet@gmail.com>
2024-01-21 23:27:38 +0600

    Created new project, set basic settings

---
+++ KirisShygysAppStore/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  KirisShygysAppAppStoreVersion
+//
+//  Created by Нурдаулет on 21.01.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ KirisShygysAppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KirisShygysAppAppStoreVersion
+//
+//  Created by Нурдаулет on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
