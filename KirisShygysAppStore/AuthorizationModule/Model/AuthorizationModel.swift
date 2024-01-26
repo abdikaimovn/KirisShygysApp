44a3ef956d1a146ff2dc195782e5687530ed67b8
Nurdaulet <abdikaimovnurdaulet@gmail.com>
2024-01-26 21:08:35 +0600

    Спроектировал слой Сети * Написал слой сети * Добавил метод для обработки ошибок при использований сети * Добавил метод для добавления нового пользователя в базу данных * Добавил метод для авторизаций * Подтянул библиотеки

---
+++ KirisShygysAppStore/AuthorizationModule/Model/AuthorizationModel.swift
@@ -0,0 +1,11 @@
+//
+//  AuthorizationModel.swift
+//  KirisShygysAppStore
+//
+//  Created by Нурдаулет on 26.01.2024.
+//
+
+struct AuthorizationModel {
+    let email: String
+    let password: String
+}
